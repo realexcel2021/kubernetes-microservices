2fd22076938c7c39bee2f2fdbf7fedf0c1268fed
Sheriff Joe Excel <realexcel2020@gmail.com>
2024-03-04 14:27:43 +0100

    changed app

---
+++ frontend/tracing.js
@@ -0,0 +1,35 @@
+  // tracing.js
+  'use strict'
+  const process = require('process');
+  const opentelemetry = require('@opentelemetry/sdk-node');
+  const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node');
+  const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
+  const { Resource } = require('@opentelemetry/resources');
+  const { SemanticResourceAttributes } = require('@opentelemetry/semantic-conventions');
+
+  
+  const exporterOptions = {
+    url: 'http://signoz-otel-collector.signoz.svc.cluster.local:4317/v1/traces'
+  }
+
+  
+  const traceExporter = new OTLPTraceExporter(exporterOptions);
+  const sdk = new opentelemetry.NodeSDK({
+    traceExporter,
+    instrumentations: [getNodeAutoInstrumentations()],
+    resource: new Resource({
+      [SemanticResourceAttributes.SERVICE_NAME]: 'frontend'
+    })
+    });
+    
+sdk.start()
+    .then(() => console.log('Tracing initialized'))
+    .catch((error) => console.log('Error initializing tracing', error));
+    
+    // gracefully shut down the SDK on process exit
+    process.on('SIGTERM', () => {
+      sdk.shutdown()
+      .then(() => console.log('Tracing terminated'))
+      .catch((error) => console.log('Error terminating tracing', error))
+      .finally(() => process.exit(0));
+      });
